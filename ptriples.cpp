856b3a0607309528e4ef0646671f11a48a0b0820
Jonathan Crombie <JonathanCrombie@outlook.com>
2014-05-04 12:15:16 -0600

    Added option "-a" to show all Pythagorean triples.

--- ptriples.cpp
+++ ptriples.cpp
@@ -1,13 +1,15 @@
 /* Public Domain.  See the LICENSE file. */
 
-/* A simple program to generate primitive pythagorean triples.   */
-/* ie.  Integer solutions to:  a^2 + b^2 = c^2                   */
-/*      where GCD(a,b,c) = 1                                     */
+/* A simple program to generate pythagorean triples.   */
+/* ie.  Integer solutions to:  a^2 + b^2 = c^2         */
+/*      where GCD(a,b,c) = 1                           */
 
 /* To compile, the GMP library needs to be already installed.    */
 /* See https://gmplib.org                                        */
 /* On linux, try:  gcc ptriples.cpp -lgmp -o ptriples            */
 
+/* A great source of info is the Wikipedia page:      */
+/* http://en.wikipedia.org/wiki/Pythagorean_triple    */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -33,33 +35,46 @@ int ttable_entry_cmpfunc( const void*, const void* );
 
 int main( int argc, char * argv[] ) {
 
-  if ( argc != 3 ) {
+  if ( argc != 3 && argc != 4 ) {
     printf("\n");
     printf("For a^2 + b^2 = c^2 :\n");
     printf("\n");
-    printf("Usage: ptriples c_min c_max\n\n");
+    printf("Usage: ptriples [-a] c_min c_max\n\n");
+    printf("Options:\n");
+    printf("  -a -- all pythagorean triples (not just primitives)\n\n");
     return 1;
   }
 
-  mpz_t c_min;
-  mpz_init_set_str( c_min,  argv[1], 10 );
+  int DoAllTriples = 0;
+  if ( argc == 4 && strcmp( argv[1], "-a" ) == 0 )
+    DoAllTriples = 1;
 
-  mpz_t c_max;
-  mpz_init_set_str( c_max,  argv[2], 10 );
+  mpz_t user_c_min;
+  mpz_init_set_str( user_c_min,  argv[argc == 3 ? 1 : 2], 10 );
 
-  if ( mpz_cmp_ui( c_min, 1 ) < 0 ) {
+  mpz_t user_c_max;
+  mpz_init_set_str( user_c_max,  argv[argc == 3 ? 2 : 3], 10 );
+
+  if ( mpz_cmp_ui( user_c_min, 1 ) < 0 ) {
     printf("\nc_min must be >= 1.  Aborting.\n\n");
-    mpz_clear( c_max );
-    mpz_clear( c_min );
+    mpz_clear( user_c_max );
+    mpz_clear( user_c_min );
     return 1;
   }
 
-  if ( mpz_cmp( c_min, c_max ) > 0 ) {
+  if ( mpz_cmp( user_c_min, user_c_max ) > 0 ) {
     printf("\nc_min must be <= c_max.  Aborting.\n\n");
-    mpz_clear( c_max );
-    mpz_clear( c_min );
+    mpz_clear( user_c_max );
+    mpz_clear( user_c_min );
     return 1;
   }
+
+  mpz_t working_c_min;
+  if ( DoAllTriples )
+    mpz_init_set_ui( working_c_min, 1 );
+  else
+    mpz_init_set( working_c_min, user_c_min );
+
 
   // We are going to use Euclid's formula:
   // For arbitrary positive integers m and n, and m > n,
@@ -86,7 +101,7 @@ int main( int argc, char * argv[] ) {
 
   // n can vary from 1 to no more than (c_max/2)^(1/2)
   mpz_t n_max;
-  mpz_init_set( n_max, c_max );
+  mpz_init_set( n_max, user_c_max );
   mpz_cdiv_q_2exp( n_max, n_max, 1 );
   mpz_sqrt( n_max, n_max );
 
@@ -108,12 +123,22 @@ int main( int argc, char * argv[] ) {
   mpz_t tempZ;
   mpz_init( tempZ );
 
+  mpz_t k;
+  mpz_init( k );
+
+  mpz_t ka;
+  mpz_init( ka );
+  mpz_t kb;
+  mpz_init( kb );
+  mpz_t kc;
+  mpz_init( kc );
+
   // iterate through n
   for ( mpz_set_ui( n, 1 );  mpz_cmp( n, n_max ) <= 0; mpz_add_ui( n, n, 1 ) ) {
     mpz_mul( n_squared, n, n );
 
     // compute m_min
-    mpz_sub( m_min, c_min, n_squared );
+    mpz_sub( m_min, working_c_min, n_squared );
 
     if ( mpz_cmp_ui( m_min, 1 ) < 0 )  // make sure mpz_sqrt() has a legal value
       mpz_set_ui( m_min, 1 );
@@ -121,7 +146,7 @@ int main( int argc, char * argv[] ) {
     mpz_sub_ui( m_min, m_min, 1 );  // subtract 1 just to be on the safe side
 
     // compute m_max
-    mpz_sub( m_max, c_max, n_squared );
+    mpz_sub( m_max, user_c_max, n_squared );
     if ( mpz_cmp_ui( m_max, 1 ) < 0 )  // make sure mpz_sqrt() has a legal value
       mpz_set_ui( m_max, 1 );
     mpz_sqrt( m_max, m_max );
@@ -140,6 +165,8 @@ int main( int argc, char * argv[] ) {
 
     // iterate through m
     for ( ; mpz_cmp( m, m_max ) <= 0; mpz_add_ui( m, m, 2 ) ) {
+
+      // generate a primitive (a,b,c)
       mpz_gcd( gcd, m, n );
       if ( mpz_cmp_ui( gcd, 1 ) != 0 )
         continue;
@@ -153,21 +180,40 @@ int main( int argc, char * argv[] ) {
 
       mpz_add( c, m_squared, n_squared );
 
-      if ( mpz_cmp( c, c_min ) < 0 )
+      // check if primitive is outside our working range
+      if ( mpz_cmp( c, working_c_min ) < 0 )
+        continue;
+      if ( mpz_cmp( c, user_c_max ) > 0 )
         continue;
 
-      if ( mpz_cmp( c, c_max ) > 0 )
-        continue;
+      if ( DoAllTriples ) {
+        // iterate through k in: (k*a)^2 + (k*b)^2 = (k*c)^2
+        mpz_fdiv_q( k, user_c_min, c );
 
-      if ( mpz_cmp( a, b ) < 0 )
-        AddPTriple( &triples, a, b, c );
+        for ( mpz_mul( kc, c, k ); mpz_cmp( kc, user_c_max ) <= 0; mpz_add_ui( k, k, 1 ), mpz_mul( kc, c, k ) ) {
+
+          if ( mpz_cmp( kc, user_c_min ) < 0 )
+            continue;
+
+          mpz_mul( ka, a, k );
+          mpz_mul( kb, b, k );
+
+          AddPTriple( &triples, ka, kb, kc );
+        }
+      }
       else
-        AddPTriple( &triples, b, a, c );
+        AddPTriple( &triples, a, b, c );
     }
   }
 
+  mpz_clear( kc );
+  mpz_clear( kb );
+  mpz_clear( ka );
+  mpz_clear( k );
+
   qsort( triples.triples, triples.count, sizeof(struct tentry), ttable_entry_cmpfunc );
 
+  // print
   long i;
   for ( i = 0; i < triples.count; i++ )
     gmp_printf("(%Zd,%Zd,%Zd)\n", triples.triples[i].a, triples.triples[i].b, triples.triples[i].c );
@@ -187,8 +233,9 @@ int main( int argc, char * argv[] ) {
 
   Cleanup_ttable( &triples );
 
-  mpz_clear( c_max );
-  mpz_clear( c_min );
+  mpz_clear( working_c_min );
+  mpz_clear( user_c_max );
+  mpz_clear( user_c_min );
 
   return 0;
 }
@@ -209,8 +256,8 @@ void AddPTriple( struct ttable* the_ttable, mpz_t a, mpz_t b, mpz_t c ) {
     memset( &the_ttable->triples[index], 0, sizeof(struct tentry) );
   }
 
-  mpz_init_set( the_ttable->triples[index].a, a );
-  mpz_init_set( the_ttable->triples[index].b, b );
+  mpz_init_set( the_ttable->triples[index].a, mpz_cmp( a, b ) < 0 ? a : b );
+  mpz_init_set( the_ttable->triples[index].b, mpz_cmp( a, b ) < 0 ? b : a );
   mpz_init_set( the_ttable->triples[index].c, c );
 }
 
